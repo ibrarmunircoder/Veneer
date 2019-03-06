8037a43ee5bc498ba4a9367f9a00ccf86b0809e1
Ibrar Munir <ibrarmunir009@gmail.com>
2019-03-06 21:53:59 +0500

    created the marketplace logic

--- script.py
+++ script.py
@@ -8,7 +8,23 @@ class Art:
   def __repr__(self):
     return "{0}. \"{1}\". {2}, {3}.".format(self.artist, self.title, self.year, self.medium)
   
+class Marketplace:
+  def __init__(self):
+    self.listings = []
+    
+  def add_listing(self, new_listing):
+    self.listings.append(new_listing)
+    
+  def remove_listing(self, expired_listing):
+    self.listings.pop(expired_listing)
+    
+  def show_listings(self):
+    for listing in self.listings:
+      print(listing)
+  
 girl_with_mandolin = Art("Picasso, Pablo", "Girl with a Mandlin (Fanny Tellier)", "oil on canvas", 1910)
 
 print(girl_with_mandolin)
-  
+
+veneer = Marketplace()
+print(veneer.show_listings())
